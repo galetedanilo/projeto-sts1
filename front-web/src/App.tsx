b39da85bab74d03e4a00047c26b35f4b208db05e
galetedanilo <galetedanilo@gmail.com>
2021-05-22 15:58:48 -0300

    Home create

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
-   <h1>Hello</h1>
+    <div>
+      <Header />
+      <Home />
+    </div>
   );
 }
 
